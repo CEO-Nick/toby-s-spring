f7015cfe5f88395a9d4b41c863472b089d8cc3fc
ikhyeon <andantej281@gmail.com>
2024-05-28 14:13:49 +0900

    오타 수정

--- src/main/java/com/study/tobyspring/user/dao/DaoFactory.java
+++ src/main/java/com/study/tobyspring/user/dao/DaoFactory.java
@@ -7,7 +7,7 @@ public class DaoFactory {
      * UserDAO 타입의 오브젝트를 어떻게 만들고, 어떻게 준비시킬지 결정
      */
     public UserDao userDao() {
-        DConnectionMaker connectionMaker = new DConnectionMaker();
+        ConnectionMaker connectionMaker = new DConnectionMaker();
         UserDao userDao = new UserDao(connectionMaker);
         return userDao;
     }
